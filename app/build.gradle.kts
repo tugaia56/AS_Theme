18c65aad418e9f0d798987bb129a98ce21229c85
Tullio <46009615+tugaia56@users.noreply.github.com>
2025-05-10 10:06:32 +0200

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,9 @@ android {
     defaultConfig {
         applicationId="tugaia56.dark.shadow.theme"
         minSdk=28
-        versionCode = 521
-        versionName = "5.2.1"
-        setProperty("archivesBaseName", "Dark_Shadow_Theme_v5.2.1")
+        versionCode = 530
+        versionName = "5.3.0"
+        setProperty("archivesBaseName", "Dark_Shadow_Theme_v5.3.0")
 
         // Themers: DO NOT MODIFY
         buildConfigField("boolean", "SUPPORTS_THIRD_PARTY_SYSTEMS", "$SUPPORTS_THIRD_PARTY_SYSTEMS")
