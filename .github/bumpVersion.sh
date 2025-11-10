ab143eff8dd468b4feef5d47a1cd57699e1c2a2f
username / Tullio Gaianigo <tugaia56#gmail.com.>
2025-11-10 22:32:01 +0100

    add scripts

--- .github/bumpVersion.sh
+++ .github/bumpVersion.sh
@@ -5,4 +5,4 @@ NEWVERCODE=${NEWVERNAME//.}
 
 sed -i 's/versionCode.*/versionCode = '$NEWVERCODE'/' app/build.gradle.kts
 sed -i 's/versionName =.*/versionName = "'$NEWVERNAME'"/' app/build.gradle.kts
-sed -i 's/Dark_Shadow_Theme_v.*/Dark_Shadow_Theme_v'$TAG'")' app/build.gradle.kts
+sed -i 's/Dark_Shadow_Theme_v.*/Dark_Shadow_Theme_v'$NEWVERNAME'")' app/build.gradle.kts
