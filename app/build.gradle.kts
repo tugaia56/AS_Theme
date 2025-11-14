6d4bb7614e1fb391e97173cd70090983625d3cd5
GitHub <ASTheme@tugaia.it>
2025-11-14 21:19:45 +0000

    Version update: Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,9 @@ android {
     defaultConfig {
         applicationId="tugaia56.dark.shadow.theme"
         minSdk=28
-        versionCode = 603
-        versionName = "6.0.3"
-        setProperty("archivesBaseName", "Dark_Shadow_Theme_v6.0.3")
+        versionCode = 604
+        versionName = "6.0.4"
+        setProperty("archivesBaseName", "Dark_Shadow_Theme_v6.0.4")
 
         // Themers: DO NOT MODIFY
         buildConfigField("boolean", "SUPPORTS_THIRD_PARTY_SYSTEMS", "$SUPPORTS_THIRD_PARTY_SYSTEMS")
